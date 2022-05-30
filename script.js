4ea524d79f55ff7983964372f8a334aceda4887f
Gino Ochoa <gochoa1088@gmail.com>
2022-05-30 10:32:26 -0700

    requestAnimationFrame implementation

--- script.js
+++ script.js
@@ -90,9 +90,6 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Remove this
-createCanvas();
-
 // Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
@@ -205,6 +202,7 @@ function animate() {
   ballMove();
   ballBoundaries();
   computerAI();
+  window.requestAnimationFrame(animate);
 }
 
 // Start Game, Reset Everything
@@ -219,21 +217,25 @@ function startGame() {
   ballReset();
   createCanvas();
   animate();
+
   canvas.addEventListener("mousemove", (e) => {
-    console.log(e.clientX);
     playerMoved = true;
+
     // Compensate for canvas being centered
     paddleBottomX = e.clientX - canvasPosition - paddleDiff;
+
     if (paddleBottomX < paddleDiff) {
       paddleBottomX = 0;
     }
+
     if (paddleBottomX > width - paddleWidth) {
       paddleBottomX = width - paddleWidth;
     }
+
     // Hide Cursor
     canvas.style.cursor = "none";
   });
 }
 
 // On Load
-// startGame();
+startGame();
